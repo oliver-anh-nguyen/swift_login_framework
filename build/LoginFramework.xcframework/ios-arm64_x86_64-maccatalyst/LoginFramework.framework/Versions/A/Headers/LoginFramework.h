ff8e0980d260ec9285e097e228f48f327a9bfe4b
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2023-01-02 12:00:57 +0700

    Build Standalone XCFramework

---
+++ build/LoginFramework.xcframework/ios-arm64_x86_64-maccatalyst/LoginFramework.framework/Versions/A/Headers/LoginFramework.h
@@ -0,0 +1,18 @@
+//
+//  LoginFramework.h
+//  LoginFramework
+//
+//  Created by AnhNguyen on 29/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoginFramework.
+FOUNDATION_EXPORT double LoginFrameworkVersionNumber;
+
+//! Project version string for LoginFramework.
+FOUNDATION_EXPORT const unsigned char LoginFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginFramework/PublicHeader.h>
+
+
